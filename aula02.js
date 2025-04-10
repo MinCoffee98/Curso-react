2b112c76067e5250f88a48621c40c111e13c53a6
MinCoffee98 <isa98vih@gmail.com>
2025-04-10 08:40:58 -0300

    React e Python

---
+++ aula02.js
@@ -0,0 +1,9 @@
+let minhaVariavel = 20
+let outraVariavel = 15
+let soma = minhaVariavel + outraVariavel
+
+console.log(soma)
+
+let segundasoma = minhaVariavel + outraVariavel + 10
+
+console.log(segundasoma)
